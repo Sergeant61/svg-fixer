91ea597784336e7ecb6da1456d1716a510b5b2ec
Sergenat61 <rec1856rec@gmail.com>
2022-04-28 10:53:36 +0300

    replacer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const SVGFixer = require('oslllo-svg-fixer');
+const replace = require('replace-in-file');
 
 const run = async () => {
   const source = './old-svg';
@@ -6,6 +7,20 @@ const run = async () => {
 
   const fixer = SVGFixer(source, destination, { showProgressBar: true, traceResolution: 1000 })
   await fixer.fix();
+
+  const options = {
+    files: './fixed-svg/*.svg',
+    from: ['fill="none"', 'stroke="none"'],
+    to: ['fill="#000"', 'stroke="#000"'],
+  };
+
+  try {
+    const results = await replace(options)
+    console.log('Replacement results:', results);
+  }
+  catch (error) {
+    console.error('Error occurred:', error);
+  }
 }
 
 run();
